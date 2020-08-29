ffe37fdd669d9f5c5a920c4f44ecc536dfcc7895
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-08-29 10:17:35 +0300

    add more tests and fix existed

--- server/src/routes/stats/__tests__/integration.stats.test.ts
+++ server/src/routes/stats/__tests__/integration.stats.test.ts
@@ -4,9 +4,10 @@ import app from "../../../server";
 import { DATE_FILTER_FORMAT } from '../../../shared//constants'
 
 describe('GET /api/stats', () => {
-    it('Should success', async () => {
-        const yesterday = moment().subtract(1, 'day').format(DATE_FILTER_FORMAT)
-        const filter = { startDate: yesterday, endDate: yesterday }
+    it('Should success and receive statistic', async () => {
+        // choose a date where statistic is present
+        const specificDay = '2020-8-27'
+        const filter = { startDate: specificDay, endDate: specificDay }
 
         const response = await request(app)
             .get('/api/stats')
@@ -28,5 +29,22 @@ describe('GET /api/stats', () => {
         expect(typeof firstElement.recovered).toBe('number')
         expect(typeof firstElement.deaths).toBe('number')
         expect(typeof firstElement.code).toBe('string')
+    })
+
+    it('Should success without statistic', async () => {
+        // there will never be statistic for the next day
+        const tomorrow = moment().format(DATE_FILTER_FORMAT)
+        const filter = { startDate: tomorrow, endDate: tomorrow }
+
+        const response = await request(app)
+            .get('/api/stats')
+            .query(filter)
+
+        const { status, body } = response
+        const firstElement = body[0]
+
+        expect(status).toBe(200)
+        expect(Array.isArray(body)).toBeTruthy()
+        expect(body.length).toBeFalsy()
     })
 })
