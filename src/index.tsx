8a9a613c7a2725b777c212c36ec825c9f01614cb
Yaroslav Fedyshyn <yaroslav.fedyshyn@nordwhale.com>
2020-09-04 18:16:26 +0300

    remove unnecessary files

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,3 @@ import { ReduxRoot } from "./ReduxRoot";
 
 const rootEl = document.getElementById("root");
 ReactDOM.render(<ReduxRoot />, rootEl);
-
-// comment in for PWA with service worker in production mode
-// registerServiceWorker();
